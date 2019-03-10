f9952df7597e4aa7658d05748e6d4e7adfa547ef
Glacial <kli-van@yandex.ru>
2019-03-11 01:15:57 +0300

    popup open/close works correctly; also placemark adding was made

--- src/js/map.js
+++ src/js/map.js
@@ -1,38 +1,78 @@
 ymaps.ready(init);
 function init() {
-  let map = new ymaps.Map("map", {
+  let map = new ymaps.Map('map', {
     center: [59.94, 30.32],
     zoom: 13,
-    controls: ["searchControl"]
+    controls: ['searchControl']
   },
     {
+      suppressObsoleteBrowserNotifier: true,
+      yandexMapDisablePoiInteractivity: true,
       suppressMapOpenBlock: true
     }
   );
+  map.behaviors.disable(['dblClickZoom']);
 
-  map.behaviors.disable(['dblClickZoom'])
+  let coords = [];
+  const inputBlock = document.querySelector('.map-input');
+  const inputHeaderText = inputBlock.querySelector('.map-input__header-text');
+  const inputFields = inputBlock.querySelectorAll('.map-input__input');
+  const inputSubmitButton = inputBlock.querySelector('.map-input__button');
 
   map.events.add('click', event => {
-    let coords = event.get('coords');
-    let input = document.querySelector(".map-input");
-    getComputedStyle(input).display === "flex" ? input.style.display = "none" : input.style.display = "flex"
-    // как найти координаты clientY clientX из этого эвента?
-    console.log(event)
+    coords = event.get('coords');
 
-    // это нам понадобится для выведения адреса в попап
-    getClickLocation(coords).then(address => console.log(address))
+    let inputPositionX = `${event.getSourceEvent().originalEvent.domEvent.originalEvent.clientX}px`
+    let inputPositionY = `${event.getSourceEvent().originalEvent.domEvent.originalEvent.clientY}px`
 
-    // let placemark = new ymaps.Placemark(coords);
-    // map.geoObjects.add(placemark);
+    // если кликаем в то же место, где только что открыли попап - закрываем его
+    if (inputPositionX === inputBlock.style.left && inputPositionY === inputBlock.style.top && inputBlock.style.display !== 'none') {
+      inputBlock.style.display = 'none';
+      return
+    }
+
+    // закрываем попап со старым адресом в заголовке
+    // позже покажем попап уже с новым адресом
+    inputBlock.style.display = 'none';
+
+    inputBlock.style.left = inputPositionX;
+    inputBlock.style.top = inputPositionY;
+    getClickLocation(coords)
+      .then(address => inputHeaderText.innerText = address)
+      .then(() => {
+        for (let i = 0; i < inputFields.length; i++) {
+          inputFields[i].value = '';
+        }
+        inputBlock.style.display = 'flex';
+      })
   })
+
+  inputSubmitButton.addEventListener('click', () => {
+    const inputName = inputFields[0].value;
+    const inputPlace = inputFields[1].value;
+    const inputFeedback = inputFields[2].value;
+
+    addPlacemark(map, coords);
+
+    inputBlock.style.display = 'none';
+    for (let i = 0; i < inputFields.length; i++) {
+      inputFields[i].value = '';
+    }
+  })
+
 }
 
 function getClickLocation(coords) {
   return new Promise((resolve, reject) => {
-    ymaps.geocode([coords[0], coords[1]]).then(res => {
+    ymaps.geocode(coords).then(res => {
       let geoObject = res.geoObjects.get(0);
       let clickLocation = geoObject.getAddressLine();
       resolve(clickLocation)
     })
   })
+}
+
+function addPlacemark(map, coords) {
+  let placemark = new ymaps.Placemark(coords);
+  map.geoObjects.add(placemark);
 }
